68b32762122ca1acb1d67fcec54a21e786b5b1b0
agoscorrea <agos_13correa@hotmail.com>
2022-12-01 07:45:47 -0300

    prueba de tomar distancia usando lat y lon pero dan mal las medidas

--- esp-now/esp-now.ino
+++ esp-now/esp-now.ino
@@ -1,6 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <Time.h>
+#define PI 3.1415926535897932384626433832795
+
+float getDistance(float,float);
 
 const char* ssid = "PruebaAgos";
 const char* password = "password";
@@ -14,7 +17,7 @@ String longitud;
 String latitudDec;
 String longitudDec;
 
-bool send = true;
+bool enviar = true;
 String success;
 bool touch = false;
 time_t t;
@@ -25,7 +28,22 @@ const int ain=A0;
 const int LED=4;
 int inputVal=0;
 
-float getDistance(){
+float getDistance(float lat2, float lon2){
+  float d = 0;
+  Serial.println("Distaance");
+  Serial.println(d);
+  float lat1 = 34.922740;
+  float lon1 = 57.8075268;
+  float φ1 = lat1 * PI/180.0; 
+  float φ2 = lat2 * PI/180.0; 
+  float Δλ = (lon2-lon1) * PI/180.0;
+  int R = 6371000.0;
+  d = acos( sin(φ1)*sin(φ2) + cos(φ1)*cos(φ2) * cos(Δλ) ) * R;
+  Serial.println("Distaance");
+  Serial.println(d);
+  }
+
+/*float getDistance(){
   int suma = 0;
   for (int i = 0; i < 200;i++){
     Serial.println(WiFi.RSSI());
@@ -40,7 +58,7 @@ float getDistance(){
   }
   Serial.print("Distance:");  
   Serial.println(suma/200);
-}
+}*/
 
 //Callback when data is sent
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus){
@@ -72,20 +90,45 @@ void OnDataRecv(uint8_t* mac, uint8_t *incomingData, uint8_t len){
   for(int i=15; i<23; i++){
     longitudDec = longitudDec + (char)incomingData[i];
     }
-  float lat = latitud.toInt() + (latitudDec.toInt()/10000000.0);
-  float lon = longitud.toInt() + (longitudDec.toInt()/10000000.0);
-  Serial.println(lat,6);
-  Serial.println(lon,6);
-  lat =0;
-  lon =0;
+  float lat2 = latitud.toInt() + (latitudDec.toInt()/10000000.0);
+  float lon2 = longitud.toInt() + (longitudDec.toInt()/10000000.0);
+  Serial.println(lat2,6);
+  Serial.println(lon2,6);
+  //float distance = getDistance(lat,lon);
+  lat2 = lat2*(PI/180.0);
+  lon2 = lon2*(PI/180.0); 
+  float d = 0;
+  float lat1 = 0;
+  float lon1 = 0;
+  float φ1 = 0;
+  float φ2 = 0;
+  float Δφ = 0;
+  float Δλ = 0;
 
-   
+  //34.9226441 57.907717"
+  lat1 = (34.9226093)*(PI/180.0);
+  lon1 = (57.9077889)*(PI/180.0);
+  float dLat = lat2 - lat1;
+  float dLon = lon2 - lon1;
+  float R = 6371000;
+  float a = 
+    sin(dLat/2) * sin(dLat/2) +
+    cos(lat1) * cos(lat2) * 
+    sin(dLon/2) * sin(dLon/2)
+    ; 
+  float c = 2*atan2(sqrt(a),sqrt(1-a)); 
+  d = c; // Distance in km
+  Serial.println("Distaance");
+  Serial.println(d,20);
+
+   lat2 =0;
+  lon2 =0;
    
 }
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
   //Wifi connection
-  Serial.begin(9600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
   //Set device as a Wi-Fi Station
@@ -102,7 +145,7 @@ void setup() {
 
   //Setting pins to touch
   pinMode(LED,OUTPUT);
-  Serial.begin(9600);
+  
   //Init ESP-NOW
   if (esp_now_init() != 0){
     Serial.println("Error initializing ESP-NOW");
@@ -129,26 +172,20 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   //Send message via ESP-NOW
-   uint8_t STR[] = "349226387579077298";
+   uint8_t STR[] = " 34.9226441 57.907717";
    
    inputVal=analogRead(ain);
+   
    /*if(inputVal>20)
     {
     Serial.println("TOCAAAAA");*/
     //esp_now_send(0, STR, 10);
-    if(send){
-    //esp_now_send(broadcastAddress2, STR, 22);
-    send = false;
+    if(enviar){
+      Serial.println("Entra");
+      //esp_now_send(broadcastAddress2, STR, 22);
+      enviar = false;
     
     }
-    
-    
-    digitalWrite(LED, HIGH);
-    delay(10000);
-    digitalWrite(LED, LOW);
-    Serial.println("Mac address: "+WiFi.macAddress());
-    //for serial monitor
-    digitalWrite(LED, LOW);
     delay(15);
   
 }
